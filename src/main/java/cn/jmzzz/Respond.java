952a797620714b73c53e53816bd11460959e34a9
Jazee <jazee@jmzzz.cn>
2020-02-25 21:47:28 +0800

    第三次提交测试 First Commit And Push

--- src/main/java/cn/jmzzz/Respond.java
+++ src/main/java/cn/jmzzz/Respond.java
@@ -1,7 +1,7 @@
 package cn.jmzzz;
 
 import com.alibaba.fastjson.JSONObject;
-//import com.sobte.cqp.jcq.event.JcqAppAbstract;1
+//import com.sobte.cqp.jcq.event.JcqAppAbstract;
 
 import java.util.Random;
 
